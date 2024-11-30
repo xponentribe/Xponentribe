717d3876e9691eec31db3a27f7668a0a9bd6b765
xponentribe <contact@xponentribe.com>
2024-11-30 11:01:33 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -148,19 +148,19 @@ const Contact = () => {
             )}
           </form>
         </div>
-        <div className="grid place-items-center p-8">
-          <iframe
-            title="Google Maps Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.967881961472!2d72.8350928!3d19.0651497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9114026a91%3A0xc53b6407e4d39f76!2sMakhija%20Arcade%2C%2035th%20Rd%2C%20Khar%20West%2C%20Mumbai%2C%20Maharashtra%20400052!5e0!3m2!1sen!2sin!4v1732940169344!5m2!1sen!2sin"
-            width="600"
-            height="350"
-            style={{ border: "0" }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+        
+         <div className="relative w-full pb-[56.25%] overflow-hidden">
+  <iframe
+    title="Google Maps Location"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.967881961472!2d72.8350928!3d19.0651497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9114026a91%3A0xc53b6407e4d39f76!2sMakhija%20Arcade%2C%2035th%20Rd%2C%20Khar%20West%2C%20Mumbai%2C%20Maharashtra%20400052!5e0!3m2!1sen!2sin!4v1732940169344!5m2!1sen!2sin"
+    className="absolute top-0 left-0 w-full h-full border-0"
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
 
-        </div>
+        
       </div>
       <Footer />
     </>
