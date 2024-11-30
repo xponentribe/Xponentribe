0fdfc25d69e42b328dfc4de27f951117a159aa96
xponentribe <contact@xponentribe.com>
2024-11-30 09:33:09 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -149,15 +149,7 @@ const Contact = () => {
           </form>
         </div>
         <div className="grid place-items-center p-8">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3773.3068188869584!2d72.8207027!3d18.962052499999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ce1374ff0223%3A0x7a482d76b3b7b69e!2sBharat%20Nagar%20Society!5e0!3m2!1sen!2sin!4v1720689120843!5m2!1sen!2sin"
-            height="350"
-            style={{ border: "0" }}
-            className="w-full"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.967881961472!2d72.8350928!3d19.065149700000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c91140262913%3A0xc53b6407e4d39f76!2sMakhija%20Arcade%2C%2035th%20Rd%2C%20Khar%2C%20Khar%20West%2C%20Mumbai%2C%20Maharashtra%20400052!5e0!3m2!1sen!2sin!4v1732939286592!5m2!1sen!2sin" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
       <Footer />
