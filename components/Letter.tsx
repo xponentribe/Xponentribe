4a00396b52eca2dda77aa104345ed394c60680b9
xponentribe <contact@xponentribe.com>
2026-02-02 22:52:13 +0530

    Update Letter.tsx

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -31,46 +31,32 @@ const Letter = () => {
         <SubscriptionForm />
 
         <div className="grid place-items-center lg:pt-16 lg:px-48 md:pt-8 md:px-24 px-8">
-          {letter.map((item) => (
-            <div
-              className="p-6 bg-white border sm:mx-8 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-12
-            "
-              key={item.id}
-            >
-              <a href="#">
-                <h5 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white sm:text-lg md:text-lg lg:text-2xl">
-                  {item.heading}
-                </h5>
-              </a>
-              <hr className="" />
-              <p className="mb-3 pt-4 font-normal text-gray-700 dark:text-gray-400 text-justify sm:text-sms">
-                {item.desc}
-              </p>
-              <p className="mb-3 font-normal text-gray-900 dark:text-gray-400">
-                {item.date}
-              </p>
-              <a
-                href={item.link}
-                target="_blank"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-400 rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
+          {[...letter]
+  .sort((a, b) => b.id - a.id)
+  .map((item) => (
+    <div
+      className="p-6 bg-white border sm:mx-8 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-12"
+      key={item.id}
+    >
+      <a href="#">
+        <h5 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white sm:text-lg md:text-lg lg:text-2xl">
+          {item.heading}
+        </h5>
+      </a>
+      <hr />
+      <p className="mb-3 pt-4 font-normal text-gray-700 dark:text-gray-400 text-justify sm:text-sms">
+        {item.desc}
+      </p>
+      <p className="mb-3 font-normal text-gray-900 dark:text-gray-400">
+        {item.date}
+      </p>
+      <a
+        href={item.link}
+        target="_blank"
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-teal-400 rounded-lg hover:bg-teal-600"
+      >
+        Read more
+      </a>
 
               {/* <div>
                 {!isSubscribed ? (
@@ -126,3 +112,4 @@ const Letter = () => {
 };
 
 export default Letter;
+
